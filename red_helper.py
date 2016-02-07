fd4e194afc37ec313da09667bca04dda9a4fc323
mangege <ciro9527@gmail.com>
2016-02-07 22:14:25 +0800

    自动点

--- red_helper.py
+++ red_helper.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 # Imports the monkeyrunner modules used by this program
+import time
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 
 # Connects to the current device, returning a MonkeyDevice object
@@ -8,13 +9,24 @@ device = MonkeyRunner.waitForConnection()
 w = 720
 h = 1280
 
-raw_input("按 Enter后再按 Ctrl+D 后开始乱点")
+#raw_input("按 Enter后再按 Ctrl+D 后开始乱点")
 print('go')
 
 while True:
-    for wi in xrange(100, w, w/6):
-        for hi in xrange(100, h, h/11):
-            device.touch(wi, hi, MonkeyDevice.DOWN_AND_UP)
+    # 抢
+    for i in xrange(0, 9):
+        device.touch(362, 692, MonkeyDevice.DOWN_AND_UP)
+        time.sleep(0.5)
+
+    # 领
+    for i in xrange(0, 2):
+        device.touch(384, 978, MonkeyDevice.DOWN_AND_UP)
+        time.sleep(0.5)
+
+    # 关
+    for i in xrange(0, 2):
+        device.touch(120, 332, MonkeyDevice.DOWN_AND_UP)
+        time.sleep(0.5)
 
 # Takes a screenshot
 #result = device.takeSnapshot()
